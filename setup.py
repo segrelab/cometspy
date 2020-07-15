6290a9c5a700c076ae8fb7a2ee778d6cbefabbea
djbajic <je.li.bre@gmail.com>
2020-07-15 17:42:04 +0200

    Fixed requirements in setup.py

--- setup.py
+++ setup.py
@@ -3,26 +3,23 @@ from distutils.core import setup
 setup(
   name='cometspy',
   packages=['cometspy'],
-  version='0.3.0',
+  version='0.3.1',
   license='MIT',
   description='The Python interface to COMETS',
   author='The COMETSPy Core Team',
   author_email='djordje.bajic@yale.edu',
   url='https://github.com/segrelab/cometspy',
-  download_url='https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url='https://github.com/segrelab/cometspy/archive/v0.3.0.tar.gz',  # New releases here!! 
   keywords=['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[
       # I get to this in a second
       'numpy',
       'cobra',
       'pandas',
-      're',
-      'subprocess',
-      'os',
       'math'],
   classifiers=[
       'Development Status :: 4 - Beta',  # "3 - Alpha", "4 - Beta", "5 - Production/Stable"
-      'Intended Audience :: Users',      # Define that your audience are developers
+      'Intended Audience :: Science/Research',      # Define that your audience are developers
       'License :: OSI Approved :: MIT License',   # Again, pick a license
       'Programming Language :: Python :: 3',      # Supported pyhton versions
       'Programming Language :: Python :: 3.4',
