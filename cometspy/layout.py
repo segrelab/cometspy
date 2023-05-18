b61f2b3ce2e2f43c95a44668b6eba893385fc4cd
Helen G Scott <helengracescott@gmail.com>
2023-05-18 14:50:38 -0400

    Add ignore_index to set_specific_metabolite

--- cometspy/layout.py
+++ cometspy/layout.py
@@ -825,7 +825,7 @@ class layout:
             newrow = pd.DataFrame([newrow], columns=newrow.keys())
             self.media = pd.concat([self.media,
                                     newrow],
-                                   axis=0, sort=False)
+                                   axis=0, sort=False, ignore_index=True)
             print('Warning: The added metabolite (' + met + ') is not ' +
                   'able to be taken up by any of the current models')
 
