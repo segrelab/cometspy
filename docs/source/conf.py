33923a37b1b6708cae66d533c235d076336699d2
Helen G Scott <helengracescott@gmail.com>
2023-08-17 19:39:06 -0400

    Add autodoc extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,10 @@ author = 'The Segre Lab at Boston University'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    # ... other extensions ...
+    'sphinx.ext.autodoc',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
