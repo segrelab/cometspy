c4d28b4766648dca2eff0b0421f7f2a35f3bf35d
Helen G Scott <helengracescott@gmail.com>
2023-08-23 12:46:48 -0400

    Get the logo working!

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,5 +35,5 @@ exclude_patterns = []
 html_theme = 'alabaster'
 html_static_path = ['_static']
 html_theme_options = {
-    'logo': '_images/cometspy_logo.png',
+    'logo': '../_images/cometspy_logo.png', # Path relative to the docs/build/_static directory
 }
