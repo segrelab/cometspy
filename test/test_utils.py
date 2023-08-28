415780da944d33d61d1b2bb37324b4e295813527
Helen G Scott <helengracescott@gmail.com>
2023-08-28 11:44:58 -0400

    Fix style with linter

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,8 +1,9 @@
 import unittest
 
-from cometspy.utils import pick_random_locations, grow_rocks, _find_unoccupied_adjacent, chemostat
+from cometspy.utils import pick_random_locations, grow_rocks, \
+    _find_unoccupied_adjacent, chemostat
 from cometspy.layout import layout
-from cometspy.params import params
+
 
 class TestUtils(unittest.TestCase):
 
@@ -16,7 +17,7 @@ class TestUtils(unittest.TestCase):
             self.assertLess(loc[0], 10)
             self.assertGreaterEqual(loc[1], 0)
             self.assertLess(loc[1], 10)
-        
+
     def test_grow_rocks(self):
         # Use the grow_rocks function to generate 5 rocks in a 5x5 grid with
         # mean size 3 and check that they are all within the grid
@@ -27,7 +28,7 @@ class TestUtils(unittest.TestCase):
             self.assertLessEqual(loc[0], 5)
             self.assertGreaterEqual(loc[1], 0)
             self.assertLessEqual(loc[1], 5)
-    
+
     def test_find_unoccupied_adjacent(self):
         # Use the _find_unoccupied_adjacent function to find all unoccupied
         # adjacent locations to a set of occupied locations in a 5x5 grid
@@ -80,15 +81,18 @@ class TestUtils(unittest.TestCase):
             self.assertTrue((loc[0], loc[1]) not in occupied_locs)
         self.assertEqual(set(unoccupied_adjacent),
                          {(0, 1), (1, 2), (2, 1), (3, 4), (4, 3), (2, 3), (1, 0), (3, 2)})
-    
+
     def test_chemostat(self):
         models = [layout(), layout()]  # Replace with actual models
         reservoir_media = {'glc__D_e': 0.01, 'nh4_e': 1000., 'pi_e': 1000.}
         dilution_rate = 0.1
         layout, parameters = chemostat(models, reservoir_media, dilution_rate)
-        self.assertAlmostEqual(parameters.all_params['metaboliteDilutionRate'], dilution_rate)
-        self.assertAlmostEqual(parameters.all_params['deathRate'], dilution_rate)
+        self.assertAlmostEqual(parameters.all_params['metaboliteDilutionRate'],
+                               dilution_rate)
+        self.assertAlmostEqual(parameters.all_params['deathRate'],
+                               dilution_rate)
         # Add more assertions related to layout and parameters
-        
+
+
 if __name__ == '__main__':
     unittest.main()
